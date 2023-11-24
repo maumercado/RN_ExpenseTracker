bf3166e9d04c247a26a61ec2e4251d4c4d8a9fc1
Mauricio Mercado <mau@maumercado.com>
2023-11-24 12:55:40 -0800

    Fix styling

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -43,7 +43,9 @@ function ExpensesOutput ({ expenses, expensesPeriod }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700
   }
 })
